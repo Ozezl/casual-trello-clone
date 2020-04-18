3e5d4724ab220225bc2b51ad16092e6db5f6d3cf
Aziz Ibragimov <ozezlus@mail.ru>
2020-04-18 02:09:09 +0200

    Updates

---
+++ precache-manifest.a9dee38e66e1bdfc3baf2dffd38f2cef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80cd0e9352a36074d8a2b6872cd15ae",
+    "url": "/casual-trello-clone/index.html"
+  },
+  {
+    "revision": "de2fc9bd80194feb97c5",
+    "url": "/casual-trello-clone/static/css/main.1db46075.chunk.css"
+  },
+  {
+    "revision": "255de0a705a8a4bf9b21",
+    "url": "/casual-trello-clone/static/js/2.150d1fe6.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/casual-trello-clone/static/js/2.150d1fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de2fc9bd80194feb97c5",
+    "url": "/casual-trello-clone/static/js/main.2f5b68c2.chunk.js"
+  },
+  {
+    "revision": "167270b4cad2f3988bc4",
+    "url": "/casual-trello-clone/static/js/runtime-main.fd0949c6.js"
+  },
+  {
+    "revision": "a6a3615c7fac0986958d5e8e05e73a39",
+    "url": "/casual-trello-clone/static/media/delete.a6a3615c.png"
+  },
+  {
+    "revision": "756ac79d37c8f32e602df1734d9ea6b4",
+    "url": "/casual-trello-clone/static/media/photo-1478428036186-d435e23988ea.756ac79d.jfif"
+  }
+]);
